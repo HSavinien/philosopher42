ebe5f000e1141f7b94e106cc19239e1a2deafdce
Theo Mongellaz <theo.mongellaz@gmail.com>
2022-10-10 16:59:21 +0200

    no more compilation error in philosopher.c

--- philosopher.c
+++ philosopher.c
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 16:37:58 by tmongell          #+#    #+#             */
-/*   Updated: 2022/09/30 13:40:37 by tmongell         ###   ########.fr       */
+/*   Updated: 2022/10/10 16:58:42 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int main (int ac, char **av)
 {
 	t_shared	shared;
-	pthread_t	supervisor_id;
 	//do parsing
 	shared = do_parsing(ac, av);
 	if (shared.error_code)
@@ -24,21 +23,17 @@ int main (int ac, char **av)
 	if (init_values(&shared))
 		return (-shared.error_code);//error management might change
 	//lock initialisation mutex
-	pthread_mutex_lock(shared.init_lock);
+	pthread_mutex_lock(&(shared.init_lock));
 	//create all philo
 	if(create_all_philo(& shared))
 		return (-shared.error_code);//error management might change
 	//create hunger thread (incremente hunger of philo, kill them when needed)
 	//create supervisor
-	if(create_supervisor(shared))
+	if(init_supervisor(&shared))
 		return (-shared.error_code);//error management might change
 	//unlock initialisation thread
-	pthread_mutex_unlock(shared.init_lock);
+	pthread_mutex_unlock(&(shared.init_lock));
 	//wait for supervisor (join)
-	pthread_join(supervisor_id, NULL);
+	pthread_join(shared.death, NULL);
 	return (0);
 }
-
-int init_values(t_shared *shared);
-int	create_all_philo(t_shared *shared);
-int create_supervisor(t_shared *shared);
